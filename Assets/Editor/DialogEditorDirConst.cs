c325638082e8e6c23f63b2cb3f275ed32371e12c
Tony <649072815@qq.com>
2023-06-30 18:08:17 +0800

    Add file path file

---
+++ Assets/Editor/DialogEditorDirConst.cs
@@ -0,0 +1,5 @@
+public static class DialogEditorDirConst
+{
+    public static readonly string SaveRootPath = "Assets/DialogSaveFile";
+    public static string GetSaveFilePath(string fileName) { return fileName; }
+}
